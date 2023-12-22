7ea5dbfa99f64987dccbbdcba97d2e8fbf34f133
nzge <76540557+nzge@users.noreply.github.com>
2023-12-21 17:40:51 -0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('changeTextButton').addEventListener('click', function() {
-    document.querySelector('h1').textContent = 'Hello, Minimalism!';
-    document.querySelector('p').textContent = 'Simplicity is the ultimate sophistication.';
-});

e8c11c17ff8ffb622c4f6ea5c7d741cd14ecc32d
nzge <76540557+nzge@users.noreply.github.com>
2023-12-21 17:23:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.querySelector('h1').textContent = 'Hello, Minimalism!';
+    document.querySelector('p').textContent = 'Simplicity is the ultimate sophistication.';
+});
